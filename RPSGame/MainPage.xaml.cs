d00dcfb750cb76f8695061d5c0435d5675af1d52
Mykyta Varnikov <varnikov@sheridancollege.ca>
2025-02-08 23:10:46 -0500

    Made some logic behind clicking buttons

--- RPSGame/MainPage.xaml.cs
+++ RPSGame/MainPage.xaml.cs
@@ -2,8 +2,46 @@
 
 public partial class MainPage : ContentPage
 {
+    private int playerScore = 0;
+    private int systemScore = 0;
+    private Random rand = new Random();
     public MainPage()
     {
         InitializeComponent();
     }
+
+    private void buttonImageClicked(object sender, EventArgs e)
+    {
+        if (sender is ImageButton button)
+        {
+            string playerChoice = button.StyleId; // or use button x:Name
+            switch (playerChoice)
+            {
+                case "Rock":
+                    playerChoiceImage.Source = ImageSource.FromFile($"rock.png");
+                    break;
+                case "Paper":
+                    playerChoiceImage.Source = ImageSource.FromFile($"paper.png");
+                    break;
+                case "Scissors":
+                    playerChoiceImage.Source = ImageSource.FromFile($"scissors.png");
+                    break;
+            }
+            int systemChoice = rand.Next(1, 4);
+            switch (systemChoice)
+            {
+                case 1:
+                    systemChoiceImage.Source = ImageSource.FromFile($"rock.png");
+                    break;
+                case 2:
+                    systemChoiceImage.Source = ImageSource.FromFile($"paper.png");
+                    break;
+                case 3:
+                    systemChoiceImage.Source = ImageSource.FromFile($"scissors.png");
+                    break;
+            }
+        }
+        
+    }
+    
 }
